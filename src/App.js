3005fc2cce9c074dba0dfaad90ea7b880315ac30
Ebenezer-ebu <eifezulike@gmail.com>
2022-07-24 21:14:09 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/HomePage";
+import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Rate from "./pages/Rate";
