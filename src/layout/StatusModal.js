9951338aee1f8b6f8b935c618a8d9b121c3b4a4a
Nurudeen Adewale <nurudeenadeolaa@gmail.com>
2022-09-10 18:08:33 +0100

    Working on update status

--- src/layout/StatusModal.js
+++ src/layout/StatusModal.js
@@ -1,29 +1,123 @@
 import React, { useState, useEffect } from "react";
+import { MdOutlineClose } from "react-icons/md";
 
-const StatusModal = ({ status, setStatus, children }) => {
-  const [open, setOpen] = useState(false);
-
+const StatusModal = ({
+  status,
+  setStatus,
+  statusId,
+  onRequest,
+  onAccepted,
+  onInProgress,
+  onCompleted,
+  onAbandoned,
+  onCancel,
+  handleStatusUpdate,
+}) => {
   const handleCloseModal = () => {
     setStatus(false);
-    setOpen(false);
   };
 
-  const childrenWithProps = React.Children.map(children, (child) => {
-    // Checking isValidElement is the safe way and avoids a typescript
-    // error too.
-    if (React.isValidElement(child)) {
-      return React.cloneElement(child, { handleCloseModal });
-    }
-    return child;
-  });
-  useEffect(() => {
-    if (status) {
-      setOpen(true);
-    } else {
-      setOpen(false);
-    }
-  }, [status]);
-  return <>{open ? <>{childrenWithProps}</> : null}</>;
+  const testing = () => {
+    onRequest();
+    handleStatusUpdate();
+    setStatus(false);
+  };
+
+  const onHandleAccepted = () => {
+    onAccepted();
+    handleStatusUpdate();
+    setStatus(false);
+  };
+
+  const onHandleInProgress = () => {
+    onInProgress();
+    handleStatusUpdate();
+    setStatus(false);
+  };
+
+  const onHandleInCompleted = () => {
+    onCompleted();
+    handleStatusUpdate();
+    setStatus(false);
+  };
+
+  const onHandleAbandoned = () => {
+    onAbandoned();
+    handleStatusUpdate();
+    setStatus(false);
+  };
+
+  const onHandleCancel = () => {
+    onCancel();
+    handleStatusUpdate();
+    setStatus(false);
+  };
+
+  return (
+    <>
+      {status && statusId && (
+        <div className="w-[375px] pb-8 absolute z-100 bg-white top-12">
+          <div className="flex justify-between w-full p-4">
+            <p>Change Status</p>
+            <MdOutlineClose
+              onClick={handleCloseModal}
+              style={{ fontSize: "20px", cursor: "pointer" }}
+            />
+          </div>
+          <hr />
+          <div className="p-4">
+            <p
+              onClick={testing}
+              className="mb-2 mt-4 text-[#FFC107] cursor-pointer"
+            >
+              Requested
+            </p>
+            <hr />
+
+            <p
+              onClick={onHandleAccepted}
+              className="mb-2 mt-4 text-[#FFC107] cursor-pointer"
+            >
+              Accepted
+            </p>
+            <hr />
+
+            <p
+              onClick={onHandleInProgress}
+              className="mb-2 mt-4 text-[#FFC107] cursor-pointer"
+            >
+              Progress
+            </p>
+            <hr />
+
+            <p
+              onClick={onHandleInCompleted}
+              className="mb-2 mt-4 text-[#14A800] cursor-pointer"
+            >
+              Completed
+            </p>
+            <hr />
+
+            <p
+              onClick={onHandleAbandoned}
+              className="mb-2 mt-4 text-[#FD2959] cursor-pointer"
+            >
+              Abandoned
+            </p>
+            <hr />
+
+            <p
+              onClick={onHandleCancel}
+              className="mb-2 mt-4 text-[#FD2959] cursor-pointer"
+            >
+              Canceled
+            </p>
+            <hr />
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default StatusModal;
--- src/pages/TutorDashboard.js
+++ src/pages/TutorDashboard.js
@@ -1,15 +1,81 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import DashboardLayout from "../layout/DashboardLayout";
 import StatusModal from "../layout/StatusModal";
 import StatusContent from "../components/StatusContent";
 import Pagination from "../components/global/Pagination";
 import AuthContext from "../context/auth/authContext";
+import axios from "../api/axios";
 
 const TutorDashboard = () => {
   const { userDetails } = useContext(AuthContext);
   const [status, setStatus] = useState(false);
+  const [sessionDetails, setSessionDetails] = useState([]);
+  const token = localStorage.getItem("userToken");
+  const user = JSON.parse(token);
+  const [selectedId, setSelectedId] = useState("");
+  const [statusValue, setStatusValue] = useState("");
+
+  console.log({selectedId, statusValue})
+
+  const onRequest = () => {
+    setStatusValue(0);
+  };
+  const onAccepted = () => {
+    setStatusValue(1);
+  };
+  const onInProgress = () => {
+    setStatusValue(2);
+  };
+  const onCompleted = () => {
+    setStatusValue(3);
+  };
+  const onAbandoned = () => {
+    setStatusValue(4);
+  };
+  const onCancel = () => {
+    setStatusValue(5);
+  };
+
+  const response = axios.get(`/Session/${user.id}/tutor`, {
+    headers: {
+      "Content-Type": "application/json",
+      accept: "*/*",
+      Authorization: `Bearer ${user.token}`,
+    },
+  });
+
+  const handleStatusUpdate = async () => {
+    console.log("HELLO THERE");
+    // e.preventDefault();
+    try {
+      await axios.patch(
+        `/Session/${selectedId}`,
+        JSON.stringify({
+          status: statusValue,
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            accept: "*/*",
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.log("ERROR", error);
+    }
+  };
+
+  useEffect(() => {
+    response
+      .then((result) => result)
+      .then((data) => {
+        setSessionDetails(data.data.data.pageItems);
+      });
+  }, []);
+
   const handleSetStatus = () => {
-    setStatus(!status);
+    setStatus(status? status : !status);
   };
   return (
     <DashboardLayout userDetails={userDetails}>
@@ -19,113 +85,92 @@ const TutorDashboard = () => {
           <h4 className="text-3xl font-bold">John Doe</h4>
         </div>
       </div>
-      <div className="px-4 lg:px-[100px] grid gap-4 md:grid-cols-2 lg:grid-cols-3 my-8">
-        <div className="bg-[#f7f7f7] text-sm pb-2 drop-shadow-lg">
-          <div className="justify-between relative">
-            <p className="bg-[#FD2959] text-white px-2 py-1 rounded absolute m-4">
-              New
-            </p>
-            <img src="/images/python.png" alt="course" className="w-full" />
-          </div>
-          <div className="flex justify-between m-4 relative">
-            <p className="font-semibold text-sm w-[188px] h-[34px]">
-              Learn Coding in Python Development fundanmental
-            </p>
-            <p
-              className="flex items-center border rounded-lg border-[#21334F] p-1 cursor-pointer"
-              onClick={handleSetStatus}
-            >
-              Status
-            </p>
-            <StatusModal status={status} setStatus={setStatus}>
-              <StatusContent />
-            </StatusModal>
-          </div>
-          <div className="flex justify-between my-2 mx-4">
-            <div className="flex justify-center items-center">
-              <img
-                className="w-[25px] rounded-full bg-black mr-2"
-                src="/images/dummy.png"
-                alt="avatar"
-              />
-              <span>Chukwudi Kamdibe</span>
+      {sessionDetails?.map((item) => (
+        <div className="px-4 lg:px-[100px] grid gap-4 md:grid-cols-2 lg:grid-cols-3 my-8 z-0">
+          <div className="bg-[#f7f7f7] text-sm pb-2 drop-shadow-lg">
+            <div className="justify-between relative">
+              <p className="bg-[#FD2959] text-white px-2 py-1 rounded absolute m-4">
+                New
+              </p>
+              <img src={item.thumbnail} alt="course" className="w-full" />
             </div>
-            <span className="text-[#758798]">Rate Student</span>
-          </div>
-          <div className="flex items-center my-2 mx-4">
-            <img src="/images/Time.png" alt="date" />
-            <span className="ml-2">July 26 - 30, 2022</span>
-          </div>
-          <div className="flex items-center my-2 mx-4">
-            <img src="/images/Progress.png" alt="progress" />
-            <span className="ml-2 text-[#FFC107]">In Progress</span>
+            <div className="flex justify-between m-4 relative">
+              <p className="font-semibold text-sm w-[188px] h-[34px]">
+                {item.topic}
+              </p>
+              <p
+                className="flex items-center border rounded-lg border-[#21334F] p-1 cursor-pointer"
+                key={item.sessionId}
+                onClick={() => {
+                  handleSetStatus();
+                  setSelectedId(item.sessionId);
+                }}
+              >
+                Status
+              </p>
+              <StatusModal
+                statusId={selectedId === item.sessionId}
+                status={status}
+                setStatus={setStatus}
+                onRequest={onRequest}
+                onAbandoned={onAbandoned}
+                onAccepted={onAccepted}
+                onCancel={onCancel}
+                onCompleted={onCompleted}
+                onInProgress={onInProgress}
+                handleStatusUpdate={handleStatusUpdate}
+              >
+                <StatusContent id={item.sessionId} />
+              </StatusModal>
+            </div>
+            <div className="flex justify-between my-2 mx-4">
+              <div className="flex justify-center items-center">
+                <img
+                  className="w-[25px] rounded-full bg-black mr-2"
+                  src={item.studentImage}
+                  alt="avatar"
+                />
+                <span>{item.student}</span>
+              </div>
+              <span className="text-[#758798]">Rate Student</span>
+            </div>
+            <div className="flex items-center my-2 mx-4">
+              <img src="/images/Time.png" alt="date" />
+              <span className="ml-2">
+                {new Date(item.startime).toLocaleDateString("en-us", {
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                  hour: "numeric",
+                })}
+                {" - "}
+                {new Date(item.endTime).toLocaleDateString("en-us", {
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                  hour: "numeric",
+                })}
+              </span>
+            </div>
+            <div className="flex items-center my-2 mx-4">
+              <img src="/images/Progress.png" alt="progress" />
+              <span className="ml-2 text-[#FFC107]">
+                {item.status === 0
+                  ? "Requested"
+                  : item.status === 1
+                  ? "Accepted"
+                  : item.status === 2
+                  ? "In Progress"
+                  : item.status === 3
+                  ? "Completed"
+                  : item.status === 4
+                  ? "Abandoned"
+                  : "Canceled"}
+              </span>
+            </div>
           </div>
         </div>
-        <div className="bg-[#f7f7f7] text-sm pb-2 drop-shadow-lg">
-          <div className="justify-between relative">
-            <img src="/images/python.png" alt="course" className="w-full" />
-          </div>
-          <div className="flex justify-between m-4">
-            <p className="font-semibold text-sm w-[188px] h-[34px]">
-              Learn Coding in Python Development fundanmental
-            </p>
-            <p className="flex items-center border rounded-lg border-[#21334F] p-1">
-              Status
-            </p>
-          </div>
-          <div className="flex justify-between my-2 mx-4">
-            <div className="flex justify-center items-center">
-              <img
-                className="w-[25px] rounded-full bg-black mr-2"
-                src="/images/dummy.png"
-                alt="avatar"
-              />
-              <span>Chukwudi Kamdibe</span>
-            </div>
-            <span className="text-[#FD2959]">Rate Student</span>
-          </div>
-          <div className="flex items-center my-2 mx-4">
-            <img src="/images/Time.png" alt="date" />
-            <span className="ml-2">July 26 - 30, 2022</span>
-          </div>
-          <div className="flex items-center my-2 mx-4">
-            <img src="/images/Success.png" alt="success" />
-            <span className="ml-2 text-[#14A800]">Completed</span>
-          </div>
-        </div>
-        <div className="bg-[#f7f7f7] text-sm pb-2 drop-shadow-lg">
-          <div className="justify-between relative">
-            <img src="/images/python.png" alt="course" className="w-full" />
-          </div>
-          <div className="flex justify-between m-4">
-            <p className="font-semibold text-sm w-[188px] h-[34px]">
-              Learn Coding in Python Development fundanmental
-            </p>
-            <p className="flex items-center border rounded-lg border-[#21334F] p-1">
-              Status
-            </p>
-          </div>
-          <div className="flex justify-between my-2 mx-4">
-            <div className="flex justify-center items-center">
-              <img
-                className="w-[25px] rounded-full bg-black mr-2"
-                src="/images/dummy.png"
-                alt="avatar"
-              />
-              <span>Chukwudi Kamdibe</span>
-            </div>
-            <span className="text-[#FD2959]">Rate Student</span>
-          </div>
-          <div className="flex items-center my-2 mx-4">
-            <img src="/images/Time.png" alt="date" />
-            <span className="ml-2">July 26 - 30, 2022</span>
-          </div>
-          <div className="flex items-center my-2 mx-4">
-            <img src="/images/Success.png" alt="success" />
-            <span className="ml-2 text-[#14A800]">Completed</span>
-          </div>
-        </div>
-      </div>
+      ))}
       <Pagination />
     </DashboardLayout>
   );
